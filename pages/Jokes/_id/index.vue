e359a28b33b0f8b1520a01309ba344084aa86b96
Adiputra <tuts.adiputra@gmail.com>
2020-07-22 14:03:22 +0700

    finish searching flow

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <nuxt-link to="/jokes">Back To Jokes List</nuxt-link>
+        <p>{{ joke }}</p>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            joke: {}
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
+            
+            console.log(res.data);
+
+            this.joke = res.data.joke;
+        } catch (err) {
+            console.log(err)
+        }
+    },
+    head() {
+        return {
+            title: 'Dad jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'index welcome'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/Jokes/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <SearchJokes v-on:search-text="searchText" />
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Joke from "@/components/Joke";
+import SearchJokes from "@/components/SearchJokes";
+
+export default {
+  components: {
+    Joke,
+    SearchJokes
+  },
+  data() {
+    return {
+      jokes: []
+    }
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
+
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+        
+        console.log(res.data);
+
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.get('https://icanhazdadjoke.com/search', config);
+      
+      console.log(res.data);
+
+      this.jokes = res.data.results;
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  head() {
+    return {
+      title: 'Dad jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'index welcome'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
