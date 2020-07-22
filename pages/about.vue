e0a64e03c3d443a63a7863888edb744703921756
Adiputra <tuts.adiputra@gmail.com>
2020-07-22 07:38:10 +0700

    init nuxt js

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is app that displays corny dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About the app',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'this is full name description'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
